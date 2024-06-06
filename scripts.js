ada6ebf00362ab74d54a185e51c687d7f237def1
Rupesh Kumar <tipu0003@gmail.com>
2024-06-06 22:43:38 +0530

    Added Education page with timeline

--- scripts.js
+++ scripts.js
@@ -12,3 +12,20 @@ function showMore() {
         behavior: 'smooth'
     });
 }
+
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
